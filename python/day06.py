6b57f1ff28e0287c566631fa2288ca7a6877e08d
xarkenz <seanedwardsclarke@gmail.com>
2024-12-06 01:20:27 -0600

    bad python for day 6

---
+++ python/day06.py
@@ -0,0 +1,84 @@
+from utils import *
+
+grid: TileMap = TileMap()
+
+with open("input/day06.txt") as input_file:
+    for row, line in enumerate(input_file.readlines()):
+        grid.put((row, 0), line.strip())
+
+init_guard_dir = Point(-1, 0)
+for point, tile in grid:
+    if tile == "^":
+        grid.put(point, ".")
+        init_guard_pos = point
+        break
+else:
+    print("no guard")
+    exit()
+
+grid1 = grid.copy()
+guard_pos = init_guard_pos
+guard_dir = init_guard_dir
+while True:
+    grid1.put(guard_pos, "X")
+    next_guard_pos = guard_pos + guard_dir
+    tile = grid.get(next_guard_pos)
+    if tile == ".":
+        guard_pos = next_guard_pos
+    elif tile == "#":
+        guard_dir = Point(guard_dir.col, -guard_dir.row)
+    else:
+        break
+
+count = 0
+for point, tile in grid1:
+    if tile == "X":
+        count += 1
+
+print(count)
+
+dir_tiles = {(1, 0): "v", (0, 1): ">", (0, -1): "<", (-1, 0): "^"}
+tile_dirs = {"v": Point(1, 0), ">": Point(0, 1), "<": Point(0, -1), "^": Point(-1, 0)}
+
+tested = TileMap()
+tested.put(init_guard_pos, "Y")
+
+loop_positions = 0
+
+def test(grid: TileMap, guard_pos: Point, guard_dir: Point):
+    while True:
+        next_guard_pos = guard_pos + guard_dir
+        tile = grid.get(next_guard_pos)
+        if tile == "." or tile in tile_dirs:
+            orig_tile = grid.get(guard_pos)
+            if orig_tile in tile_dirs and tile_dirs[orig_tile] == guard_dir:
+                return True
+            grid.put(guard_pos, dir_tiles[guard_dir.row, guard_dir.col])
+            guard_pos = next_guard_pos
+        elif tile == "#":
+            guard_dir = Point(guard_dir.col, -guard_dir.row)
+        else:
+            return False
+
+grid2 = grid.copy()
+guard_pos = init_guard_pos
+guard_dir = init_guard_dir
+while True:
+    next_guard_pos = guard_pos + guard_dir
+    tile = grid.get(next_guard_pos)
+    if tile == "#":
+        guard_dir = Point(guard_dir.col, -guard_dir.row)
+    else:
+        if tested.get(next_guard_pos) != "Y" and grid2.get(next_guard_pos) == ".":
+            test_grid = grid2.copy()
+            test_grid.put(next_guard_pos, "#")
+            if test(test_grid, guard_pos, guard_dir):
+                tested.put(next_guard_pos, "Y")
+                loop_positions += 1
+        if tile == ".":
+            grid2.put(guard_pos, dir_tiles[guard_dir.row, guard_dir.col])
+            guard_pos = next_guard_pos
+        else:
+            break
+
+print(loop_positions)
---
+++ python/utils.py
@@ -0,0 +1,145 @@
+from dataclasses import dataclass
+from typing import Iterator
+
+def gcd(a: int, b: int) -> int:
+    while b > 0:
+        (a, b) = (b, a % b)
+    return a
+
+def lcm(a: int, b: int) -> int:
+    return a // gcd(a, b) * b
+
+@dataclass
+class Point:
+    row: int
+    col: int
+
+    def manhattan_distance_to(self, other) -> int:
+        return abs(self.row - other[0]) + abs(self.col - other[1])
+
+    def __getitem__(self, index: int) -> int:
+        return [self.row, self.col].__getitem__(index)
+
+    def __iter__(self) -> Iterator[int]:
+        return [self.row, self.col].__iter__()
+
+    def __add__(self, other) -> "Point":
+        return Point(self.row + other[0], self.col + other[1])
+    
+    def __sub__(self, other) -> "Point":
+        return Point(self.row - other[0], self.col - other[1])
+    
+    def __mul__(self, scalar: int) -> "Point":
+        return Point(self.row * scalar, self.col * scalar)
+
+class TileMapRow:
+    def __init__(self, filler_tile: str = " "):
+        self.filler_tile: str = filler_tile
+        self.tiles: str = ""
+        self.first_col: int = 0
+    
+    def min_col(self) -> int:
+        return self.first_col
+    
+    def max_col(self) -> int:
+        return self.first_col + len(self.tiles) - 1
+
+    def col_range(self) -> range:
+        return range(self.first_col, self.first_col + len(self.tiles))
+    
+    def get(self, col: int) -> str:
+        if self.min_col() <= col <= self.max_col():
+            return self.tiles[col - self.first_col]
+        else:
+            return self.filler_tile
+    
+    def __getitem__(self, col) -> str:
+        if isinstance(col, slice):
+            start = col.start if col.start is not None else (self.min_col() if col.step is None or col.step > 0 else self.max_col())
+            stop = col.stop if col.stop is not None else (self.max_col() + 1 if col.step is None or col.step > 0 else self.min_col() - 1)
+            step = col.step if col.step is not None else 1
+            return "".join(self.get(slice_col) for slice_col in range(start, stop, step))
+        else:
+            return self.get(col)
+
+    def put(self, col: int, tiles: str) -> str:
+        if len(self.tiles) == 0:
+            self.first_col = col
+            self.tiles = tiles
+        else:
+            # eugh
+            new_tiles = ""
+            if self.first_col < col:
+                new_first_col = self.first_col
+                new_tiles += self.tiles[:min(len(self.tiles), col - self.first_col)]
+                if new_first_col + len(new_tiles) < col:
+                    new_tiles += self.filler_tile * (col - (new_first_col + len(new_tiles)))
+            else:
+                new_first_col = col
+            new_tiles += tiles
+            if new_first_col + len(new_tiles) < self.first_col + len(self.tiles):
+                if new_first_col + len(new_tiles) < self.first_col:
+                    new_tiles += self.filler_tile * (self.first_col - (new_first_col + len(new_tiles)))
+                new_tiles += self.tiles[max(0, (new_first_col + len(new_tiles)) - self.first_col):]
+            self.first_col = new_first_col
+            self.tiles = new_tiles
+    
+    def copy(self) -> "TileMapRow":
+        copy = TileMapRow(self.filler_tile)
+        copy.tiles = self.tiles
+        copy.first_col = self.first_col
+        return copy
+
+class TileMap:
+    def __init__(self, filler_tile: str = " "):
+        self.filler_tile: str = filler_tile
+        self.rows: list[TileMapRow] = []
+        self.first_row: int = 0
+        self.cached_min_col: int = 0
+        self.cached_max_col: int = -1
+    
+    def min_row(self) -> int:
+        return self.first_row
+    
+    def max_row(self) -> int:
+        return self.first_row + len(self.rows) - 1
+
+    def min_col(self) -> int:
+        return self.cached_min_col
+    
+    def max_col(self) -> int:
+        return self.cached_max_col
+
+    def get(self, point) -> str:
+        row = point[0]
+        col = point[1]
+        if self.min_row() <= row <= self.max_row():
+            return self.rows[row].get(col)
+        else:
+            return self.filler_tile
+    
+    def put(self, point, tiles: str):
+        row = point[0]
+        col = point[1]
+        if row > self.max_row():
+            self.rows.extend(TileMapRow(self.filler_tile) for _ in range(row - self.max_row()))
+        elif row < self.min_row():
+            self.rows[0:0] = (TileMapRow(self.filler_tile) for _ in range(self.min_row() - row))
+        self.rows[row].put(col, tiles)
+        self.cached_min_col = min(self.cached_min_col, self.rows[row].min_col())
+        self.cached_max_col = max(self.cached_max_col, self.rows[row].max_col())
+    
+    def copy(self) -> "TileMap":
+        copy = TileMap(self.filler_tile)
+        copy.rows = [row.copy() for row in self.rows]
+        copy.first_row = self.first_row
+        copy.cached_min_col = self.cached_min_col
+        copy.cached_max_col = self.cached_max_col
+        return copy
+    
+    def __iter__(self) -> Iterator[tuple[Point, str]]:
+        points_iter = (Point(self.first_row + row_index, col) for row_index in range(len(self.rows)) for col in self.rows[row_index].col_range())
+        return ((point, self.get(point)) for point in points_iter)
+    
+    def __str__(self) -> str:
+        return "\n".join(tile_row[self.min_col() : self.max_col() + 1] for tile_row in self.rows)
