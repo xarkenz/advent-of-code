830737e3665de32d831933d8c2f1b49b1d1f965b
xarkenz <seanedwardsclarke@gmail.com>
2024-12-10 12:03:17 -0600

    simplified day 10 solution

--- python/day10.py
+++ python/day10.py
@@ -7,56 +7,34 @@ directions: list[Point] = [
     Point(0, -1),
 ]
 
-grid = get_input_tile_map("day10.txt")
+grid: TileMap = get_input_tile_map("day10.txt")
 
 trailheads: list[Point] = [point for point, tile in grid if tile == "0"]
 
-scores = 0
+scores_total: int = 0
+ratings_total: int = 0
 
 for trailhead in trailheads:
-    score = 0
-    frontier: set[Point] = {trailhead}
-    last_frontier_size = 0
-    while last_frontier_size != len(frontier):
-        last_frontier_size = len(frontier)
-        for point in frontier.copy():
-            orig_tile = grid.get(point)
+    trail_ends: set[Point] = set()
+    frontier: list[Point] = [trailhead]
+    while frontier:
+        new_frontier: list[Point] = []
+        for point in frontier:
+            tile = grid.get(point)
             for direction in directions:
                 new_point = point + direction
-                if new_point in frontier:
-                    continue
                 new_tile = grid.get(new_point)
-                if ord(new_tile) == ord(orig_tile) + 1:
+                if ord(new_tile) == ord(tile) + 1:
                     if new_tile == "9":
-                        score += 1
-                    frontier.add(new_point)
-    scores += score
+                        if new_point not in trail_ends:
+                            scores_total += 1
+                            trail_ends.add(new_point)
+                        ratings_total += 1
+                    else:
+                        new_frontier.append(new_point)
+        frontier = new_frontier
 
-print(scores)
-
-scores = 0
-
-def count_paths(path: list[Point]) -> int:
-    point = path[-1]
-    orig_tile = grid.get(point)
-    count = 0
-    for direction in directions:
-        new_point = point + direction
-        new_tile = grid.get(new_point)
-        if ord(new_tile) == ord(orig_tile) + 1:
-            if new_tile == "9":
-                count += 1
-            else:
-                path.append(new_point)
-                count += count_paths(path)
-    path.pop()
-    return count
-
-for trailhead in trailheads:
-    path: list[Point] = [trailhead]
-    score = count_paths(path)
-    scores += score
-
-print(scores)
+print("[day10p1] Trailhead score total:", scores_total)
+print("[day10p2] Trailhead rating total:", ratings_total)
 
 print_time_elapsed()
