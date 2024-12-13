d7060033ec390a1f8c2f8113d89029da3f4ed216
xarkenz <seanedwardsclarke@gmail.com>
2024-12-13 01:20:25 -0600

    improved day 13 solution

--- python/day13.py
+++ python/day13.py
@@ -1,85 +1,45 @@
 from utils import *
 
-def sign(x: int) -> int:
-    return 0 if x == 0 else x // abs(x)
-
-def cmpdirs(p1: Point, p2: Point) -> int:
-    return p1.row * p2.col - p1.col * p2.row
-
-def reaches(p1: Point, p2: Point) -> bool:
-    rdiv, rmod = divmod(p2.row, p1.row)
-    if rmod != 0:
-        return False
-    cdiv, cmod = divmod(p2.col, p1.col)
-    if cmod != 0:
-        return False
-    return rdiv == cdiv
-
-cases: list[list[Point]] = []
+scenarios: list[list[Point]] = []
 for line in get_input_lines("day13.txt"):
     if line.startswith("Button A"):
         x, y = line[12:].split(", Y+")
-        cases.append([Point(int(x), int(y))])
+        scenarios.append([Point(int(x), int(y))])
     elif line.startswith("Button B"):
         x, y = line[12:].split(", Y+")
-        cases[-1].append(Point(int(x), int(y)))
+        scenarios[-1].append(Point(int(x), int(y)))
     elif line.startswith("Prize"):
         x, y = line[9:].split(", Y=")
-        cases[-1].append(Point(int(x), int(y)))
+        scenarios[-1].append(Point(int(x), int(y)))
 
-# tokens = 0
-# for a_move, b_move, prize in cases:
-#     for a_times in range(100):
-#         for b_times in range(100):
-#             claw = a_move * a_times + b_move * b_times
-#             if claw == prize:
-#                 tokens += 3 * a_times + b_times
-#                 break
-#         if claw == prize:
-#                 break
-# print(tokens)
+def calculate_tokens(a_move: Point, b_move: Point, prize_pos: Point) -> int:
+    ax, ay = a_move.row, a_move.col
+    bx, by = b_move.row, b_move.col
+    px, py = prize_pos.row, prize_pos.col
+    determinant = ax * by - bx * ay
+    a_presses_numerator = by * px - bx * py
+    b_presses_numerator = ax * py - ay * px
+    a_presses, a_remainder = divmod(a_presses_numerator, determinant)
+    b_presses, b_remainder = divmod(b_presses_numerator, determinant)
+    if a_remainder == 0 and b_remainder == 0:
+        return a_presses * 3 + b_presses * 1
+    else:
+        return 0
 
-# tokens = 0
-# for a_move, b_move, prize in cases:
-#     prize += Point(10000000000000, 10000000000000)
-#     a_dir = cmpdirs(a_move, prize)
-#     b_dir = cmpdirs(b_move, prize)
-#     if sign(a_dir) == sign(b_dir):
-#         continue
-#     elif b_dir == 0 and a_dir == 0:
-#         print("oh no")
-#         continue
-#     elif b_dir == 0:
-#         if reaches(b_move, prize):
-#             tokens += prize.row // b_move.row
-#         continue
-#     elif a_dir == 0:
-#         if reaches(a_move, prize):
-#             tokens += 3 * (prize.row // a_move.row)
-#         continue
-#     down_move, up_move, down_tokens, up_tokens = (a_move, b_move, 3, 1) if a_dir > b_dir else (b_move, a_move, 1, 3)
-#     current_pos = down_move
-#     current_tokens = down_tokens
-#     unreachable = False
-#     print(f"{prize=}")
-#     while not reaches(current_pos, prize):
-#         current_dir = cmpdirs(current_pos, prize)
-#         if current_dir > 0:
-#             current_pos += up_move
-#             current_tokens += up_tokens
-#         elif current_dir < 0:
-#             current_pos += down_move
-#             current_tokens += down_tokens
-#         else:
-#             unreachable = True
-#             break
-#     if unreachable:
-#         print("unreachable")
-#         continue
-#     times = prize.row // current_pos.row
-#     tokens += times * current_tokens
-#     print("done")
-# print(tokens)
+token_count_p1: int = 0
+token_count_p2: int = 0
+
+for a_move, b_move, prize_pos in scenarios:
+    token_count_p1 += calculate_tokens(a_move, b_move, prize_pos)
+    prize_pos += Point(10000000000000, 10000000000000)
+    token_count_p2 += calculate_tokens(a_move, b_move, prize_pos)
+
+print("[day13p1] Tokens required:", token_count_p1)
+print("[day13p2] Tokens required:", token_count_p2)
+
+print_time_elapsed()
+
+# My first solution works and I'm still proud of it so I'm keeping it here
 
 # def reducefrac(frac):
 #     g = gcd(abs(frac[0]), abs(frac[1]))
@@ -122,20 +82,3 @@ for line in get_input_lines("day13.txt"):
 #     if y1[1] == 1 and y2[1] == 1 and y1[0] >= 0 and y2[0] >= 0:
 #         tokens += 3 * y1[0] + y2[0]
 # print(tokens)
-
-tokens = 0
-for a_move, b_move, prize in cases:
-    prize += Point(10000000000000, 10000000000000)
-    ax, ay = a_move.col, a_move.row
-    bx, by = b_move.col, b_move.row
-    px, py = prize.col, prize.row
-    d = ax * by - bx * ay
-    an = by * px - bx * py
-    bn = ax * py - ay * px
-    a, arem = divmod(an, d)
-    b, brem = divmod(bn, d)
-    if arem == 0 and brem == 0:
-        tokens += 3 * a + b
-print(tokens)
-
-print_time_elapsed()
