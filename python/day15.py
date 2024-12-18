26e2194330a72cc9e727f2f1c2ee083804d94263
xarkenz <seanedwardsclarke@gmail.com>
2024-12-17 22:27:24 -0600

    cleaned up day 15 somewhat

--- python/day15.py
+++ python/day15.py
@@ -1,9 +1,22 @@
 from utils import *
 
-dir_chars = {"^": Point(-1, 0), ">": Point(0, 1), "<": Point(0, -1), "v": Point(1, 0)}
+dir_chars: dict[str, Point] = {
+    "v": Point(1, 0),
+    ">": Point(0, 1),
+    "^": Point(-1, 0),
+    "<": Point(0, -1),
+}
 
-grid = get_input_tile_map("day15.txt")
-moves = [dir_chars[c] for c in get_input_text("day15_move.txt") if c in dir_chars]
+grid: TileMap = TileMap()
+moves: list[Point] = []
+in_moves_section: bool = False
+for row, line in enumerate(get_input_lines("day15.txt")):
+    if not line:
+        in_moves_section = True
+    elif in_moves_section:
+        moves.extend(dir_chars[char] for char in line if char in dir_chars)
+    else:
+        grid.put((row, 0), line)
 
 grid_p2 = TileMap()
 for point, tile in grid:
@@ -44,49 +57,46 @@ for move in moves:
             grid.put(target, ".")
             robot = target
 
-gps = 0
+gps_coord_sum: int = 0
 for point, tile in grid:
     if tile == "O":
-        gps += 100 * point.row + point.col
-print(grid)
-print(gps)
+        gps_coord_sum += 100 * point.row + point.col
 
-grid = grid_p2 #lazy
-robot = robot_p2
+print("[day15p1] Sum of GPS coordinates:", gps_coord_sum)
 
-def print_grid():
+def print_grid(grid: TileMap, robot: Point):
     grid.put(robot, "@")
     print(grid)
     grid.put(robot, ".")
 
-def move_robot_p2(move):
-    global robot
-    target = robot + move
-    target_tile = grid.get(target)
+def move_robot_p2(move: Point):
+    global robot_p2
+    target: Point = robot_p2 + move
+    target_tile: str = grid_p2.get(target)
     if target_tile == ".":
-        robot = target
+        robot_p2 = target
     elif target_tile in "[]":
         if move.row == 0:
-            to_shift = [(target, target_tile)]
-            test = target + move
-            test_tile = grid.get(test)
+            to_shift: list[tuple[Point, str]] = [(target, target_tile)]
+            test: Point = target + move
+            test_tile: str = grid_p2.get(test)
             while test_tile in "[]":
                 to_shift.append((test, test_tile))
                 test = test + move
-                test_tile = grid.get(test)
+                test_tile = grid_p2.get(test)
             if test_tile == ".":
                 while to_shift:
                     test, test_tile = to_shift.pop()
-                    grid.put(test + move, test_tile)
-                grid.put(target, ".")
-                robot = target
+                    grid_p2.put(test + move, test_tile)
+                grid_p2.put(target, ".")
+                robot_p2 = target
         else:
-            tests = [target]
-            to_shift = []
-            clear_to_move = True
+            tests: list[Point] = [target]
+            to_shift: list[tuple[Point, str]] = []
+            clear_to_move: bool = True
             while tests:
-                test = tests.pop()
-                test_tile = grid.get(test)
+                test: Point = tests.pop()
+                test_tile: str = grid_p2.get(test)
                 if test_tile == "[":
                     tests.append(test + move)
                     tests.append(test + move + (0, 1))
@@ -103,20 +113,20 @@ def move_robot_p2(move):
             if clear_to_move:
                 while to_shift:
                     test, test_tile = to_shift.pop()
-                    grid.put(test + move, test_tile)
-                    if grid.get(test) == test_tile:
-                        grid.put(test, ".")
-                robot = target
+                    grid_p2.put(test + move, test_tile)
+                    if grid_p2.get(test) == test_tile:
+                        grid_p2.put(test, ".")
+                robot_p2 = target
 
 for move in moves:
     move_robot_p2(move)
 
-gps = 0
-for point, tile in grid:
+gps_coord_sum: int = 0
+for point, tile in grid_p2:
     if tile == "[":
-        gps += 100 * point.row + point.col
-print_grid()
-print(gps)
+        gps_coord_sum += 100 * point.row + point.col
+
+print("[day15p2] Sum of scaled-up GPS coordinates:", gps_coord_sum)
 
 # i = input()
 # while i:
