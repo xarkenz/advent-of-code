5373e4c3aa5d16cfafd005fc2b187709e42e0c44
xarkenz <seanedwardsclarke@gmail.com>
2024-12-18 17:32:52 -0600

    fixed gitignore for rust

--- 2024/rust/target/debug/deps/rust-b9d7508424704fdb.d
+++
@@ -1,8 +0,0 @@
-C:\Users\seane\Projects\adventofcode-2024\rust\target\debug\deps\librust-b9d7508424704fdb.rmeta: src/main.rs src\utils.rs src\solution\mod.rs src\solution\day01.rs
-
-C:\Users\seane\Projects\adventofcode-2024\rust\target\debug\deps\rust-b9d7508424704fdb.d: src/main.rs src\utils.rs src\solution\mod.rs src\solution\day01.rs
-
-src/main.rs:
-src\utils.rs:
-src\solution\mod.rs:
-src\solution\day01.rs:
