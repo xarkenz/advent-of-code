6c5d18bb95c95f5a3ceb3cf727ecde51333efc19
xarkenz <seanedwardsclarke@gmail.com>
2025-12-07 01:25:52 -0600

    added 2025 day 7

--- 2025/python/day07.py
+++ 2025/python/day07.py
@@ -2,9 +2,22 @@ from utils import *
 
 start_time: float = get_start_time()
 
-#
+# Mapping from X-coordinate to the number of beams in that spot in different timelines
+beams: dict[int, int] = {}
+split_count: int = 0
 
-print("[07p1] :")
-print("[07p2] :")
+for line in get_input_lines("day07.txt"):
+    for x, ch in enumerate(line):
+        if ch == "S":
+            beams[x] = 1
+        elif ch == "^" and x in beams:
+            count = beams[x]
+            del beams[x]
+            beams[x - 1] = beams.get(x - 1, 0) + count
+            beams[x + 1] = beams.get(x + 1, 0) + count
+            split_count += 1
+
+print("[07p1] Number of splits:", split_count)
+print("[07p2] Number of timelines:", sum(beams.values()))
 
 print_time_elapsed(start_time)
